d2f8161201b74e2ba2e574eddfc5e42916a39a86
ceisutb19 <raydiazvega@hotmail.com>
2018-05-09 12:59:53 -0500

    Paquetes actualizados

---
+++ app/src/main/java/com/ceisutb/tictactoe/TicTacToeApplication.java
@@ -0,0 +1,13 @@
+package com.ceisutb.tictactoe;
+
+import android.app.Application;
+
+import com.facebook.FacebookSdk;
+
+public class TicTacToeApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FacebookSdk.sdkInitialize(this);
+    }
+}
---
+++ app/src/main/java/com/ceisutb/tictactoe/model/Vector2.java
@@ -0,0 +1,11 @@
+package com.ceisutb.tictactoe.model;
+
+public final class Vector2 {
+    public Vector2(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float x;
+    public float y;
+}
